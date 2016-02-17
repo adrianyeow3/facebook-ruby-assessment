64aa6d6d51799144473ce485b1c7d3f6727ca4b5
Adrian <adrianyeow3@gmail.com>
2016-02-17 12:55:18 +0800

    Added signup and login

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,3 +1,44 @@
 get '/' do
   erb :"static/index"
 end
+
+post '/signup' do
+	user = User.new(params[:user])
+	if user.save
+		session[:user_id] = user.id 
+		redirect "/users/#{user.id}"
+	else
+		erb :'/static/error'
+	end
+end
+
+post '/login' do
+	@user = User.find_by(email: params[:user][:email])
+	if @user && @user.authenticate(params[:user][:password])
+		session[:user_id] = @user.id
+		redirect "/users/#{@user.id}"
+	else
+		erb :'/static/error'
+	end
+end
+
+post '/logout' do
+	session[:user_id] = nil
+	redirect '/'
+end
+
+get "/users/:user_id" do
+	@user = User.find(params[:user_id])
+	@posts = Post.all
+	erb :'/static/profile'
+end
+
+post '/comment' do
+	@posts = current_user.posts.new(my_post: params[:user][:comment])
+	if @posts.save
+		redirect "/users/#{current_user.id}"
+	else
+		redirect "/users/#{current_user.id}"
+	end
+end
+
---
+++ app/helpers/user.rb
@@ -0,0 +1,16 @@
+helpers do
+	def current_user
+		if session[:user_id]
+			if @current_user
+				@current_user
+			else
+				@current_user = User.find_by_id(session[:user_id])
+			end
+			# @current_user ||= User.find_by_id(session[:user_id])
+		end
+	end
+
+	def logged_in?
+		!current_user.nil?
+	end
+end
