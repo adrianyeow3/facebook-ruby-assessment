c8c56bd62628206ee077d04c7951cfb84de366f9
Adrian <adrianyeow3@gmail.com>
2016-02-17 13:29:03 +0800

    Added user_id in post table

--- db/migrate/20160217110800_create_posts.rb
+++ db/migrate/20160217110800_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration
 	def change
 	    create_table :posts do |t|
 	      t.string :my_post
+	      t.integer :user_id
 	      t.timestamps null: false
 	    end
     end
